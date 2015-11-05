37b582eb59391c262d4888fc6e72ce5aaeb91c3c
Grant Timmerman <granttimmerman@gmail.com>
2015-11-05 09:46:23 -0800

    Finish github demo

--- github/index.js
+++ github/index.js
@@ -1,20 +1,38 @@
 var request = require('request');
-
+var atob = require('atob');
 
 /**
  * Gets an api url string given a GitHub user and repo.
  */
 function getURL(user, repo) {
-  return 'http://api.github.com/repos/' + user + '/' + repo + '/readme';
+  return 'https://api.github.com/repos/' + user + '/' + repo + '/readme';
 }
 
-function getReadmeData() {
+function getReadmeData(cb) {
   var url = getURL('grant', 'talks');
-  request(url, function (error, response, body) {
+  var options = {
+    url: url,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+  request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(body); // Show the HTML for the Google homepage.
+      var json = JSON.parse(body)
+      cb(json);
     }
   });
 }
 
-getReadmeData();
+/**
+ * Processes the GitHub API data
+ */
+function processReadmeData() {
+  getReadmeData(function(data) {
+    var readme = atob(data.content);
+    console.log(readme);
+  });
+}
+
+processReadmeData();
+
