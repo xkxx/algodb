e340ff1f4545eab800f1dbf28ea2262c3352e52e
xkxx <xukexiang@gmail.com>
2015-11-12 20:30:45 -0800

    try again... npm dump

--- lang-pkgs/npm/get-desc.js
+++ lang-pkgs/npm/get-desc.js
@@ -3,7 +3,7 @@ var fetch = require('node-fetch');
 fetch.Promise = bluebird;
 var path = require('path');
 var downloadUrl = "https://api.npmjs.org/downloads/point/last-month/";
-var npmStat = require('npm-stats')(); //('http://127.0.0.1:5984/', {modules: 'npm'});
+var npmStat = require('npm-stats')('http://127.0.0.1:5984/');
 var fs = bluebird.promisifyAll(require('fs'));
 
 var matchGithub = /github.com\/([\w\d-_]*)\/([\w\d-_]*)/;
@@ -22,16 +22,18 @@ var getDescForPkg = function(pkgName) {
 
   return bluebird.all([
     pkg.infoAsync(),
-    pkg.starsAsync(),
     getDownloadsForPkg(pkgName)
   ]).then(function(res) {
     var json = res[0];
-    var stars = res[1].length;
-    var downloads = res[2];
+    var downloads = res[1];
 
     var name = json.name;
     var desc = json.description;
     var keywords = json.keywords;
+
+    var time = json.time;
+    var ctime = time? json.time.created : json.ctime;
+    var mtime = time? json.time.modified : json.mtime;
 
     var verLatest = json['dist-tags'].latest;
 
@@ -43,12 +45,14 @@ var getDescForPkg = function(pkgName) {
     // find repo
     var repository = json.repository;
     var github;
-    if (repository) {
+
+    if (repository && repository.url) {
       github = repository.url.match(matchGithub);
     }
-    else {
+    else if (typeof json.homepage == 'string') {
       github = json.homepage.match(matchGithub);
     }
+
     if (github) {
       github = github.slice(1, 3);
     }
@@ -56,6 +60,8 @@ var getDescForPkg = function(pkgName) {
     return {
       name: name,
       desc: desc,
+      timeUpdated: mtime,
+      timeCreated: ctime,
       latest: verLatest,
       keywords: keywords,
       readme: readme,
@@ -63,7 +69,6 @@ var getDescForPkg = function(pkgName) {
       readmeType: readmeType,
       repo: repository? repository.url : null,
       github: github,
-      stars: stars,
       downloads: downloads
     };
   });
@@ -73,8 +78,14 @@ exports.getDescForPkg = getDescForPkg;
 
 var moduleListAsync = bluebird.promisify(npmStat.list);
 var getAllPkgs = function() {
-  return moduleListAsync();
+  return fetch('http://localhost:5984/registry/_all_docs')
+  .then((res) => res.json())
+  .then(function(res) {
+    return res.rows.map((item)=>item.key);
+  });
 };
+
+exports.getAllPkgs = getAllPkgs;
 
 var readOrCreateList = function() {
   var file = 'list.json';
@@ -85,10 +96,10 @@ var readOrCreateList = function() {
   })
   .catch(function() {
     var list;
-    return getAllPkg()
+    return getAllPkgs()
     .then(function(content) {
       list = content;
-      return fs.writeFileAsync(file, content, 'utf-8');
+      return fs.writeFileAsync(file, JSON.stringify(list), 'utf-8');
     })
     .then(function() {
       return list;
@@ -100,7 +111,7 @@ var readCursor = function() {
   var file = 'cursor.dat';
   return fs.readFileAsync(file, 'utf-8')
   .then(function(content) {
-    return content;
+    return parseInt(content,10);
   })
   .catch(function(err) {
     return -1;
@@ -109,27 +120,38 @@ var readCursor = function() {
 
 var updateCursor = function(cur) {
   var file = 'cursor.dat';
-  return fs.writeFileAsync(file, cur.toString(), 'utf-8')
+  return fs.writeFileAsync(file, cur.toString(), 'utf-8');
 };
 
-var process = function() {
+var main = function() {
   var out = bluebird.promisifyAll(fs.createWriteStream('results.json', {flags: 'a'}));
+  var list, cursor;
 
   readOrCreateList()
-  .then(function(list) {
-    return readCursor()
-    .then(function(cursor) {
-      return list.slice(cursor + 1);
-    });
+  .then(function(r) {
+    list = r;
+    return readCursor();
   })
-  .each(function(pkgName, idx, len) {
-    console.info(idx + " of " + len);
+  .then(function(r) {
+    cursor = r + 1;
+    return list.slice(cursor);
+  })
+  .each(function(pkgName, idx) {
+    if (pkgName.startsWith("_design/")) {
+        console.info("Skipping design doc " + pkgName);
+        return;
+    }
+    console.info(pkgName + " " + (cursor + idx) + " of " + list.length);
     return getDescForPkg(pkgName)
     .then(function(result) {
       return out.writeAsync(JSON.stringify(result) + '\n');
     })
     .then(function() {
-      return updateCursor(idx);
+      return updateCursor(cursor + idx);
+    })
+    .catch(function(error) {
+      console.error(error);
+      throw error;
     });
   })
   .then(function() {
@@ -141,6 +163,9 @@ var process = function() {
 };
 
 if(require.main === module) {
+  main();
+  /*
+
   var pkgName = process.argv.slice(-1)[0];
   if (pkgName) {
     getDescForPkg(pkgName)
@@ -150,7 +175,5 @@ if(require.main === module) {
     .catch(function(err) {
       console.error(err);
     });
-  }
+  } */
 }
-
-exports.getAllPkgs = getAllPkgs;
