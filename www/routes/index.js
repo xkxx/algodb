c1a2fd159cb12807f53c4d7069885a20c3b32855
Grant Timmerman <granttimmerman@gmail.com>
2015-11-21 00:23:47 -0800

    Add results

--- www/routes/index.js
+++ www/routes/index.js
@@ -4,9 +4,14 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var query = req.query.q;
-  res.render('index', {
-    query: query
-  });
+  var results = [];
+
+  var data = {
+    query: query,
+    results: results
+  };
+
+  res.render('index', data);
 });
 
 module.exports = router;
