e77938f17457314a44815d8c720a4ce800ca8590
Grant Timmerman <granttimmerman@gmail.com>
2015-11-29 00:49:54 -0500

    Add basic styling to homepage

---
+++ www/private/db.js
@@ -0,0 +1,68 @@
+/**
+ * The interface to the database. Used for searching and storing.
+ */
+
+var async = require('async');
+var request = require('request');
+var map = require('objmap');
+
+var ELASTIC_SEARCH_URL = 'http://localhost:9200/throwtable/';
+
+/**
+ * Quick way to get json from a url.
+ * @param  {String}   url The URL to request JSON from.
+ * @param  {Callback} cb  The callback
+ * @return {Object[]} err, body
+ */
+function getJSON(url, cb) {
+  request(url, function(err, res, body) {
+    cb(err, JSON.parse(body));
+  });
+}
+
+module.exports = {
+  /**
+   * Searches the elastic search db.
+   * @param  {String} query The raw text query
+   * @param  {Callback} cb The response callback
+   * @return {Object} res A search result object with the following data
+   * @return {Object} res.error The error
+   * @return {Object} res.json The json body from elasticsearch
+   */
+  search: function(query, cb) {
+    var url = ELASTIC_SEARCH_URL;
+    request(url, function(error, response, body) {
+      var res = {};
+      res.error = error;
+
+      // Add the response data if there wasn't an error
+      if (!error && response.statusCode == 200) {
+        res.json = JSON.parse(body);
+      }
+
+      // Send back the data
+      cb(error, res);
+    });
+  },
+
+  /**
+   * Gets the counts of the "algorithm", "category", and "implementation" mappings.
+   * Assumes never fails.
+   * @param  {Callback} cb The response callback
+   * @return {Object}   res["algorithm", "category", "implementation"] The number of documents in this index
+   */
+  count_summary: function(cb) {
+    var urlBase = ELASTIC_SEARCH_URL;
+    // TODO(grant) make prettier using ["algorithm", "category", "implementation"]
+    async.parallel({
+      algorithm: function(c) { getJSON(urlBase + 'algorithm/_count', c); },
+      category: function(c) { getJSON(urlBase + 'category/_count', c); },
+      implementation: function(c) { getJSON(urlBase + 'implementation/_count', c); },
+    }, function(err, data) {
+      var mappedData = map(data, function(countObj) {
+        return countObj.count;
+      });
+      cb(err, mappedData);
+    });
+  }
+};
--- www/routes/index.js
+++ www/routes/index.js
@@ -1,17 +1,31 @@
+var async = require('async');
 var express = require('express');
 var router = express.Router();
+
+var db = require('../private/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var query = req.query.q;
-  var results = [];
 
-  var data = {
-    query: query,
-    results: results
-  };
+  // Send the queries to the db.
+  // Remember that all the callbacks return the error object then the result!
+  async.parallel({
+    count_summary: function(cb) {
+      db.count_summary(cb);
+    },
+    search: function(cb) {
+      db.search(query, cb);
+    }
+  }, function(err, data) {
+    // After doing all the queries in parallel, render the page!
 
-  res.render('index', data);
+    data.query = query; // may be undefined
+    data.results = []; // temp
+    console.log('Render /index.html with the following data:');
+    console.log(data);
+    res.render('index', data);
+  });
 });
 
 module.exports = router;
