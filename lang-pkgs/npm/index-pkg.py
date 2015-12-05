12d83e29c839cda8c606cb179a170ae8834771bc
xkxx <xukexiang@gmail.com>
2015-12-05 15:08:28 -0800

    incremental work on getting nel to work on npm

--- lang-pkgs/npm/index-pkg.py
+++ lang-pkgs/npm/index-pkg.py
@@ -1,57 +1,80 @@
+import sys, os
+sys.path.append(os.path.join(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))), 'NEL'))
 from elasticsearch import Elasticsearch
-import sys
-sys.path.append('../../NEL')
-
-import stringmatch
 import json
 import time
 import math
+import stringmatching_npm as stringmatch
+
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
-dump = open('results.json')
-
 def get_text_content(pkg):
-  return [(pkg['desc'], 2.0)
-          (pkg['readme'], 1.0)
-         ]
+    return [
+        (pkg['desc'], 2.0),
+        (pkg['readme'], 1.0)
+    ]
 
 def match_valid(algo, score, weight):
-  if score > 1.0:
-    return True
+    if score > 1.0:
+        return True
 
 def get_es_id(pkg):
-  return 'npm:%s:js' % pkg['name']
+    return 'npm:%s:js' % pkg['name']
 
 def compute_pkg_weight(pkg):
-  return pkg['downloads'] * 1.0 / math.log(int(time.time() * 1000) - pkg['timeUpdated'])
+    return pkg['downloads'] * 1.0 / math.log(int(time.time() * 1000) - pkg['timeUpdated'])
 
 def add_to_db(pkg, impls):
-  es.index(index=INDEX_NAME,
+    es.index(index='throwtable',
     doc_type='implementation',
     id=get_es_id(pkg),
     body={
-      'language': 'js',
-      'algorithm': [impls],
-      'source': 'npm',
-      'description': pkg['desc'],
-      'instruction': {
-         'package': pkg['name']
-         'command': 'npm install ' + pkg['name']
-         'content': pkg['readme']
-      },
-      'popularity': compute_pkg_weight(pkg)
+        'language': 'js',
+        'algorithm': [impls],
+        'source': 'npm',
+        'description': pkg['desc'],
+        'instruction': {
+            'package': pkg['name'],
+            'command': 'npm install ' + pkg['name'],
+            'content': pkg['readme']
+        },
+        'popularity': compute_pkg_weight(pkg)
 
-  })
+    })
 
-def main():
-  for line in dump:
-    pkg = json.loads(line)
+def get_links(pkg):
     texts = get_text_content(pkg)
     impls = []
     for (text, weight) in texts:
-      result = stringmatch.stringmatch(text)
-      for (algo, score) in result:
-        if match_valid(algo, score, weight):
-          impls.append(algo)
+        result = stringmatch.link_algorithm(text, es)
+        for (algo, score) in result:
+            if match_valid(algo, score, weight):
+                impls.append(algo)
+
+def index_package(line):
+    pkg = json.loads(line)
+    impls = get_links(pkg)
     if len(impls) > 0:
-      add_to_db(pkg, impls)
+        add_to_db(pkg, impls)
+
+def work():
+    dump = open('results.json')
+    for line in dump:
+        index_package(line)
+
+def test_query(desp):
+    from pprint import pprint
+    pprint(stringmatch.link_algorithm(desp, es), width=1)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        work()
+    else:
+        from subprocess import check_output
+        from pprint import pprint
+        line = check_output(["node", "get-desc.js", sys.argv[1]])
+        print line
+        pkg = json.loads(line)
+        impls = get_links(pkg)
+        pprint(impls, width=1)
