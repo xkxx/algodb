e8c459cdf26ddba2a371da06b66726277619eda3
Minjing Zhu <minjiz@uw.edu>
2015-12-13 20:19:11 -0800

    deleting old eval script

--- Evaluation/pick_nel_queries.py
+++
@@ -1,9 +0,0 @@
-import random
-
-allqueries = set(line for line in open('algolist.txt'))
-output = open('nel_queries.txt', 'w+')
-
-allqueries = list(allqueries)
-random.shuffle(allqueries)
-for query in allqueries[:50]:
-    output.write(query)
