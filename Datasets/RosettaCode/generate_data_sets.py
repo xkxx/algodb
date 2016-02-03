6314189de8b80fd8fb1f456a90f07ef18c42c6bf
Minjing Zhu <minjiz@uw.edu>
2016-02-02 21:03:18 -0800

    ranking model

--- Datasets/RosettaCode/generate_data_sets.py
+++ Datasets/RosettaCode/generate_data_sets.py
@@ -1,7 +1,21 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 import mwclient as mw
-import mwparserfromhell as parser
+from cassandra.cluster import Cluster
 
-site = mw.Site('rosettacode.org', path='/mw/')
 
-for page in site.Pages['Category:Programming Tasks']:
-    print page.page_title
+
+def store_rosettacode_cassandra():
+    site = mw.Site('rosettacode.org', path='/mw/')
+    cluster = Cluster(['127.0.0.1'])  # localhost
+    session = cluster.connect()  # default key space
+    session.set_keyspace('rosettacode')
+
+    for page in site.Pages['Category:Programming Tasks']:
+        session.execute("INSERT INTO queries (anchor, cprob, entity) VALUES (%s, %s, %s)", [anchor.encode('utf8').decode('utf8'), num(cprob), entity.encode('utf8').decode('utf8')])
+
+# TODO
+def store_label_redis():
+    pass
---
+++ RankingModel/Classifier.py
@@ -0,0 +1,4 @@
+from sklearn import svm
+
+# support vector regression: features -> ranking score
+clf = svm.LinearSVC()
