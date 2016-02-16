a2e815b1a015d0410409d8dc5d78c6a906687e2d
xkxx <xukexiang@gmail.com>
2016-02-15 21:22:52 -0800

    added working linearsvm ranking

--- RankingModel/Classifier.py
+++ RankingModel/Classifier.py
@@ -1,57 +1,67 @@
-# # for task data
-# from cassandra.cluster import Cluster
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 # for algorithm
 from elasticsearch import Elasticsearch
+from Algorithm import get_corresponding_algo, get_all_mentioned_algo
+
+# for rosettacode impl
+from cassandra.cluster import Cluster
+from Implementation import get_all_impls
 
 # for training set labels
 import redis
 
 # using support vector regression: features -> ranking score
 from sklearn import svm
-clf = svm.LinearSVR()
 
 # for randomly sampling negative training example
 import random
 
 # feature extract
-from fuzzywuzzy import fuzz
-from simhash import Simhash
+from FeatureExtractor import extract_features
 
 def train():
     CORRESPONDING = 1.0
     NON_CORRESPONDING = 0.0
+    clf = svm.LinearSVR()
+
+    # cassandra init
+    cluster = Cluster(['127.0.0.1'])  # localhost
+    session = cluster.connect()  # default key space
+    session.set_keyspace('rosettacode')
 
     # for task names and labels
     rd = redis.StrictRedis(host='localhost', port=6379, db=0)
     # for algo names and data
     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
-    # get all task names in rosettacode
-    task_names = rd.hkeys('rosettacode-label-algoname')
-    # get all algo names in elasticsearch algorithm table
-    query = {
-        "query": {
-            "match_all" : {}
-        }
-    }
-    res = es.search(index='throwtable', doc_type='algorithm', body=query)
-    algo_names = [entry['name'] for entry in res['hits']]
+    tasks = get_all_impls(session, rd)
+    # # get all task names in rosettacode
+    # task_names = rd.hkeys('rosettacode-label-algoname')
+    # # get all algo names in elasticsearch algorithm table
+    # query = {
+    #     "query": {
+    #         "match_all" : {}
+    #     }
+    # }
+    # res = es.search(index='throwtable', doc_type='algorithm', body=query)
+    algo_names = get_all_mentioned_algo(rd)
 
     # debugging
-    print '# of task_names:', len(task_names)
+    print '# of task_names:', len(tasks)
     print '# of algo_names:', len(algo_names)
 
     # feature vector
     feature_vector = list()
     # score vector
     score_vector = list()
+    print algo_names
 
-    for task_name in task_names:
-        algo_name = rd.hget('rosettacode-label-algoname', task_name)
+    for task in tasks:
         # now only train on tasks that are algorithms, and have wiki pages
-        if not rd.sismember('rosettacode-label-isalgo', task_name) \
-            or len(algo_name) == 0:
+        if not task.rank_trainable():
             continue
 
         # TODO: not using algorithm's data for now
@@ -64,26 +74,36 @@ def train():
 
         # positive:negative = 1:1
         # positive training example
-        feature_vector.append(mock_extractor(task_name, algo_name))
+        corres_algo = get_corresponding_algo(task.label)
+        feature_vector.append(extract_features(task, corres_algo))
         score_vector.append(CORRESPONDING)
+
         # negative training example
-        random_algo = random.choice(algo_names)
-        while (random_algo == algo_name): # until we get a negative example
-            random_algo = random.choice(algo_names)
-        feature_vector.append(mock_extractor(task_name, random_algo))
+        random_algo_name = None
+        while (random_algo_name is None or random_algo_name == task.label):
+            random_algo_name = random.choice(algo_names)
+        random_algo = get_corresponding_algo(random_algo_name)
+        feature_vector.append(extract_features(task, random_algo))
         score_vector.append(NON_CORRESPONDING)
 
     # train
     clf.fit(feature_vector, score_vector)
+    return clf
 
 # samples_features = a list of features, size = # of samples * # of features
 # returns a list of
-def classify(samples_features):
+def classify(model, samples_features):
     return predict(samples_features)
 
-def mock_extractor(title1, title2):
-    features = list()
-    features.append(fuzz.ratio(title1, title2))
-    features.append(fuzz.partial_ratio(title1, title2))
-    features.append(Simhash(title1).distance(Simhash(title2)))
-    return features
+# def mock_extractor(title1, title2):
+#     features = list()
+#     features.append(fuzz.ratio(title1, title2))
+#     features.append(fuzz.partial_ratio(title1, title2))
+#     features.append(Simhash(title1).distance(Simhash(title2)))
+#     return features
+
+def main():
+    train()
+
+if __name__ == '__main__':
+    main()
---
+++ RankingModel/Implementation.py
@@ -0,0 +1,41 @@
+from cassandra.query import SimpleStatement
+
+class Implementation:
+    def __init__(self, page_title, categories, iwlinks, text,
+            label=None, is_algo=False):
+        self.title = page_title
+        self.categories = categories
+        self.iwlinks = iwlinks
+        self.text = text
+        self.label = label
+        self.is_algo = is_algo
+
+    def rank_trainable(self):
+        return self.label is not None and self.is_algo
+
+def process_single_impl(row, rd):
+    impl = Implementation(row.page_title, row.categories, row.iwlinks, row.text)
+    # get corresponding wiki page
+    algo_name = rd.hget('rosettacode-label-algoname', impl.title)
+    if algo_name:
+        algo_name = algo_name.strip()
+        algo_name = algo_name if len(algo_name) > 0 else None
+    # get if task is algo
+    is_algo = rd.sismember('rosettacode-label-isalgo', impl.title)
+    # record everything
+    impl.label = algo_name
+    impl.is_algo = is_algo
+    # commit
+    return impl
+
+def get_all_impls(cas, rd):
+    """
+        cas: cassandra session
+        rd: redis connection
+    """
+    query = "SELECT * FROM rosettacode"
+    statement = SimpleStatement(query, fetch_size=100)
+    results = []
+    for row in cas.execute(statement):
+        results.append(process_single_impl(row, rd))
+    return results
